31c04ab147c484c0dc2feffb936de7071c0de1b8
Be-student <presentsong@naver.com>
2022-11-17 16:09:58 +0900

    feat(Domain): 이동 키 추가

---
+++ src/main/java/bridge/domain/Moving.java
@@ -0,0 +1,22 @@
+package bridge.domain;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public enum Moving {
+    UP("U"),
+    DOWN("D");
+
+    private final String move;
+
+    Moving(String move) {
+        this.move = move;
+    }
+
+    public static Moving from(String move) {
+        return Arrays.stream(Moving.values())
+                .filter(it -> Objects.equals(it.move, move))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("U,D 만 입력 가능합니다"));
+    }
+}
