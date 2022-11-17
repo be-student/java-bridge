aca80269303fb0a1e369b53d8fd308cb3bb352f5
Be-student <presentsong@naver.com>
2022-11-17 16:10:44 +0900

    refactor(BridgeMaker): 람다로 호출식 변경

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -20,7 +20,7 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return IntStream.generate(() -> bridgeNumberGenerator.generate())
+        return IntStream.generate(bridgeNumberGenerator::generate)
                 .limit(size)
                 .mapToObj(this::toOneBridge)
                 .collect(Collectors.toList());
