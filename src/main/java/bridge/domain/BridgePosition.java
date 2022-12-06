f57c33431111bfd074ac5e64ed8d277a95452943
Be-student <presentsong@naver.com>
2022-12-06 20:44:51 +0900

    feat: BridgeMaker 클래스를 이용해 다리 생성 기능을 완성한다

---
+++ src/main/java/bridge/domain/BridgePosition.java
@@ -0,0 +1,27 @@
+package bridge.domain;
+
+import java.util.Arrays;
+
+public enum BridgePosition {
+    UP(1, "U"),
+    DOWN(0, "D");
+
+    private final int generatedCode;
+    private final String position;
+
+    BridgePosition(int generatedCode, String text) {
+        this.generatedCode = generatedCode;
+        position = text;
+    }
+
+    public static BridgePosition createWithCode(int code) {
+        return Arrays.stream(BridgePosition.values())
+                .filter(it -> it.generatedCode == code)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("코드는 1 또는 0만 가능합니다"));
+    }
+
+    public String getPosition() {
+        return position;
+    }
+}
