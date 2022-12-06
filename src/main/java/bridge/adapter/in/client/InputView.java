aa606b10e603dd5fc9cfd32a7ac6e8abccb5d6f1
Be-student <presentsong@naver.com>
2022-12-06 22:14:26 +0900

    feat: 다리 길이를 입력받는 기능을 추가한다

--- src/main/java/bridge/adapter/in/client/InputView.java
+++ src/main/java/bridge/adapter/in/client/InputView.java
@@ -1,15 +1,21 @@
 package bridge.adapter.in.client;
 
+import bridge.application.port.in.CreateBridgeCommand;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private static final String ASK_BRIDGE_MESSAGE = "다리의 길이를 입력해주세요.";
+
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public CreateBridgeCommand readBridgeSize() {
+        System.out.println(ASK_BRIDGE_MESSAGE);
+        return new CreateBridgeCommand(Console.readLine());
     }
 
     /**
