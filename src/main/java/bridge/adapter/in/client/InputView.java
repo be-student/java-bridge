adc30ba80c631a4d959aa20b76044cfeb1b495b3
Be-student <presentsong@naver.com>
2022-12-07 00:59:11 +0900

    feat: 다시 시도할 지 여부 입력받는 기능을 추가한다

--- src/main/java/bridge/adapter/in/client/InputView.java
+++ src/main/java/bridge/adapter/in/client/InputView.java
@@ -11,6 +11,7 @@ public class InputView {
 
     private static final String ASK_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String ASK_BRIDGE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String ASK_GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -31,7 +32,8 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public GameCommand readGameCommand() {
+        System.out.println(ASK_GAME_COMMAND_MESSAGE);
+        return GameCommand.from(Console.readLine());
     }
 }
