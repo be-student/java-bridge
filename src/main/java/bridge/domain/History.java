1274ab5b9463ead61cd1533665632c700472845a
Be-student <presentsong@naver.com>
2022-12-07 20:30:11 +0900

    feat: 기록 기능 추가

---
+++ src/main/java/bridge/domain/History.java
@@ -0,0 +1,53 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class History {
+
+    private List<List<String>> history;
+    private int triedCount = 0;
+
+    public History() {
+        init();
+    }
+
+    private void init() {
+        history = new ArrayList<>();
+        history.add(new ArrayList<>());
+        history.add(new ArrayList<>());
+    }
+
+    public void retry() {
+        triedCount++;
+        init();
+    }
+
+    public int getTriedCount() {
+        return triedCount;
+    }
+
+    public void move(BridgePosition nextPosition, Bridge bridge) {
+        int length = history.get(0).size();
+        put(nextPosition, bridge.isEqualAtIndex(length, nextPosition));
+    }
+
+    private void put(BridgePosition nextPosition, boolean isSuccess) {
+        int index = nextPosition.toCode();
+        int other = 1 - index;
+        if (isSuccess) {
+            history.get(index).add("O");
+            history.get(other).add(" ");
+            return;
+        }
+        history.get(index).add("X");
+        history.get(other).add(" ");
+    }
+
+    public List<List<String>> toDto() {
+        List<List<String>> result = List.of(new ArrayList<>(), new ArrayList<>());
+        result.get(0).addAll(history.get(0));
+        result.get(1).addAll(history.get(1));
+        return result;
+    }
+}
